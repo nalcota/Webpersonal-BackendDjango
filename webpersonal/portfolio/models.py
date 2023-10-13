6c5d8a63c8df4c95271f21914be50eea1c21830a
nalcota <ni.alcota@duoucuc.cl>
2023-10-12 23:38:04 -0300

    Se Agrega el proyecto

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Pagina Web", blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de emisión")
+
+
+    class Meta: 
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.title
